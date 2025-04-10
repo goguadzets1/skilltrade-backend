02fc30bd4d74a4603f81816667cf58d1f18958de
goguadzets1 <ts.goguadze13@gmail.com>
2025-04-10 23:57:45 +0200

    Fix vercel domain

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://skilltrade-frontend-five.vercel.app/"],
+    allow_origins=["https://skilltrade-frontend-five.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
